db868b4b6f1182fa5bdb68f221f632f6b4242d34
Jason Lind <jason.lind@groupbobaloo.com>
2017-12-03 17:55:45 -0600

    transformations

---
+++ StratML.Transform.Core/ITransformOneToTwo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StratML.Core.One;
+using StratML.Core.Two;
+
+namespace StratML.Transform.Core
+{
+    public interface ITransformOneToTwo
+    {
+        PerformancePlanOrReport Transform(StrategicPlan plan);
+    }
+    public interface ITransformTwoToOne
+    {
+        StrategicPlan Transform(PerformancePlanOrReport plan);
+    }
+}
---
+++ StratML.Transform/TransformOneToTwo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StratML.Core.One;
+using StratML.Core.Two;
+using StratML.Transform.Core;
+using StratML.Core;
+
+namespace StratML.Transform
+{
+    public class TransformOneToTwo : ITransformOneToTwo
+    {
+        public PerformancePlanOrReport Transform(StrategicPlan plan)
+        {
+            return plan.CreateRelatedInstance<PerformancePlanOrReport>();
+        }
+    }
+    public class TransformTwoToOne : ITransformTwoToOne
+    {
+        public StrategicPlan Transform(PerformancePlanOrReport plan)
+        {
+            return plan.CreateRelatedInstance<StrategicPlan>();
+        }
+    }
+}
