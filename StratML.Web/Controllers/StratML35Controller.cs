20db822e5e77fd448673e08a4f668f9d656a196e
Jason Lind <jason.lind@groupbobaloo.com>
2017-12-19 20:21:00 -0600

    35 file serv up

---
+++ StratML.Web/Controllers/StratML35Controller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StratML.Core.ThreeFive;
+using System.Threading;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using StratML.Core;
+
+namespace StratML.Web.Controllers
+{
+    public class StratML35Controller : Controller
+    {
+        protected Uri ServicesURI { get; private set; }
+        protected IConfiguration Config { get; private set; }
+        public StratML35Controller(Uri servicesUri, IConfiguration config)
+        {
+            this.ServicesURI = servicesUri;
+            this.Config = config;
+        }
+        [HttpGet]
+        [Route("StratML35/{fileName}")]
+        public async Task<IActionResult> Index(string fileName, CancellationToken token = default(CancellationToken))
+        {
+            CloudStorageAccount acct = CloudStorageAccount.Parse(Config["StorageAccountConnectionString"]);
+            var fileClient = acct.CreateCloudFileClient();
+            var awsfiles = fileClient.GetShareReference("stratml-35");
+            var root = awsfiles.GetRootDirectoryReference();
+            var file = root.GetFileReference(fileName);
+            OrganizationalStrategyDocument doc = null;
+            if (await file.ExistsAsync())
+            {
+                using (var streamReader = new StreamReader(await file.OpenReadAsync()))
+                {
+                    doc = XmlHelper.Deserialize<OrganizationalStrategyDocument>(
+                        await streamReader.ReadToEndAsync());
+                }
+            }
+            return View(doc);
+        }
+    }
+}
